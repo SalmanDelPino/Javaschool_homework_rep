b4a4c433fb1a5e989df3681813b7325fea71da9b
SalmanDelPino <mrsalmandp@gmail.com>
2026-07-12 23:48:47 +0200

    Junit examless added

---
+++ src/test/java/tests/examples/ExampleTest.java
@@ -0,0 +1,16 @@
+package tests.examples;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void firstTest() {
+        System.out.println("Amerika ya!");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("Hallo! Hallo! Hallo!");
+    }
+}
