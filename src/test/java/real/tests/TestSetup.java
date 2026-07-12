da8122059f9ce8181b817448f1fad8484b5334e1
SalmanDelPino <mrsalmandp@gmail.com>
2026-07-13 00:55:33 +0200

    testSetup import fixed

--- src/test/java/real/tests/TestSetup.java
+++ src/test/java/real/tests/TestSetup.java
@@ -2,12 +2,13 @@ package real.tests;
 
 import org.junit.jupiter.api.BeforeAll;
 
-import java.lang.module.Configuration;
+import com.codeborne.selenide.Configuration;
+
 
 public class TestSetup {
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = "chrome";
-        Configuration.timeout = "10000"; // обычно 4000
+        Configuration.timeout = 10000; // обычно 4000
     }
 }
